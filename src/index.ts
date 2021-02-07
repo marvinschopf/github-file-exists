dc537c108344fb5aef7a0c12ad36eafccc70b15a
Marvin Schopf <marvin@schopf.biz>
2021-02-07 02:21:18 +0100

    Initial Version

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,21 @@
-import * as https from "https";
+import fetch from "node-fetch";
 
-export default async function existsAsync(
+export default async function exists(
 	owner: string,
 	repo: string,
 	path: string
 ): Promise<boolean> {
-	return await performRequest(buildRequest(owner, repo, path));
-}
-
-function buildRequest(
-	owner: string,
-	repo: string,
-	path: string
-): https.RequestOptions {
-	return {
-		hostname: "api.github.com",
-		port: 443,
-		path: `/repos/${owner}/${repo}/contents/${path}`,
-		headers: {
-			Accept: "application/vnd.github.v3+json",
-			"User-Agent": "github-file-exists"
-		},
-	};
-}
-
-function performRequest(options: https.RequestOptions): Promise<boolean> {
-	return new Promise((resolve, reject) => {
-		https
-			.request(options, function (response) {
-				const { statusCode, headers } = response;
-				if (statusCode !== 200) {
-					resolve(false);
-				}
-				const chunks = [];
-				response.on("data", (chunk) => {
-					chunks.push(chunk);
-				});
-				response.on("end", () => {
-					resolve(true);
-				});
-			})
-			.end();
-	});
+	return (
+		await fetch(
+			`https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+			{
+				headers: {
+					Accept: "application/vnd.github.v3+json",
+					"User-Agent": "github-file-exists",
+				},
+			}
+		)
+	).status === 200
+		? true
+		: false;
 }
