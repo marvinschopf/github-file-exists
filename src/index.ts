a92dd187c51ce7e549d421bf923434527b86ce7b
Marvin Schopf <marvin@schopf.biz>
2021-02-07 02:48:52 +0100

    Add better error reporting and authentication

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,29 @@ import fetch from "node-fetch";
 export default async function exists(
 	owner: string,
 	repo: string,
-	path: string
+	path: string,
+	apiKey?: string
 ): Promise<boolean> {
-	return (
-		await fetch(
-			`https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-			{
-				headers: {
-					Accept: "application/vnd.github.v3+json",
-					"User-Agent": "github-file-exists",
-				},
-			}
-		)
-	).status === 200
-		? true
-		: false;
+	let headers = {
+		Accept: "application/vnd.github.v3+json",
+		"User-Agent": "github-file-exists",
+	};
+	if (apiKey) {
+		headers["Authorization"] = `token ${apiKey ? apiKey : ""}`;
+	}
+	const response: Response = await fetch(
+		`https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+		{
+			headers: headers,
+		}
+	);
+	const responseJson = await response.json();
+	if (response.status != 200 && response.status != 400) {
+		if (responseJson.message) {
+			throw new Error(`Error: ${responseJson.message}`);
+		} else {
+			throw new Error(`Error: ${response.status} ${response.statusText}`);
+		}
+	}
+	return response.status === 200 ? true : false;
 }
